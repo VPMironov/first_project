fc33e1c81f424513c63487ca47e6e46b7a50ed0a
VPMironov <46045611+VPMironov@users.noreply.github.com>
2018-12-21 12:47:58 +0300

    11
    Первая версия

---
+++ mobile/config.php
@@ -0,0 +1,9 @@
+<?
+# bd parametr
+define('prefix', 'an_');
+$db_host = 'localhost'; # Имя хоста БД
+$db_name = 'u0432667_silatel'; # Имя базы данных
+$db_user = 'u0432667_silatel'; # Пользователь БД
+$db_pass = '3N1s7E2u'; # Пароль к БД
+$db_charset = 'utf8';
+?>
