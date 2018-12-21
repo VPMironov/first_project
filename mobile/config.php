d39b0d5d2262f0503a50a0ad2a740daa68730c02
VPMironov <46045611+VPMironov@users.noreply.github.com>
2018-12-21 13:01:15 +0300

    Update config.php

--- mobile/config.php
+++ mobile/config.php
@@ -2,8 +2,8 @@
 # bd parametr 1
 define('prefix', 'an_');
 $db_host = 'localhost'; # Имя хоста БД
-$db_name = 'u0432667_silatel'; # Имя базы данных
-$db_user = 'u0432667_silatel'; # Пользователь БД
-$db_pass = '3N1s7E2u'; # Пароль к БД
+$db_name = 'u043266l'; # Имя базы данных
+$db_user = 'u043266l'; # Пользователь БД
+$db_pass = ''; # Пароль к БД
 $db_charset = 'utf8';
 ?>
