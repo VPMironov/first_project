9372737d92e9e09f7b2b7441e73264e287da3ecc
VPMironov <46045611+VPMironov@users.noreply.github.com>
2018-12-21 13:35:07 +0300

    Update config.php

--- mobile/config.php
+++ mobile/config.php
@@ -1,9 +1,9 @@
 <?
-# bd parametr 1
+# bd parametr
 define('prefix', 'an_');
 $db_host = 'localhost'; # Имя хоста БД
 $db_name = 'u043266l'; # Имя базы данных
 $db_user = 'u043266l'; # Пользователь БД
-$db_pass = ''; # Пароль к БД
+$db_pass = '3N'; # Пароль к БД
 $db_charset = 'utf8';
 ?>
